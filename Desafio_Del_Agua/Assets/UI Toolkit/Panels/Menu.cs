2da978ff4c542848f6ece63ff58d48e8a7529b72
Urspectre <162526090+Urspectre@users.noreply.github.com>
2024-04-05 19:32:14 -0500

    Metodos de botones
    Se agrega el metodo para el control del menu principal

--- Desafio_Del_Agua/Assets/UI Toolkit/Panels/Menu.cs
+++ Desafio_Del_Agua/Assets/UI Toolkit/Panels/Menu.cs
@@ -6,21 +6,43 @@ using UnityEngine.UIElements;
 public class MiCodigo : MonoBehaviour
 {
     private UIDocument _document;
-    private Button _button;
+    private Button _button1;
+    private Button _button2;
+   // private Button _button3;
+   //private Button _button4;
+   //--------------------------------------------------
+    public string Uno;
+    public string Options;
 
+    
+   //------------------------------------------------
     private void Awake()
     {
         _document = GetComponent<UIDocument>();
-        _button = _document.rootVisualElement.Q<Button>("play") as Button;
-        
-        _button.RegisterCallback<ClickEvent>(OnPlayGameClick);
+        _button1 = _document.rootVisualElement.Q<Button>("play") as Button;
+        _button1.RegisterCallback<ClickEvent>(OnPlayGameClick);
+
+        _button2 = _document.rootVisualElement.Q<Button>("options") as Button;
+        _button2.RegisterCallback<ClickEvent>(OnOptionsClick);
+
+
     }
     private void OnDisable()
     {
-        _button.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+        _button1.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+        _button2.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+        //_button3.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+        //_button4.UnregisterCallback<ClickEvent>(OnPlayGameClick);
     }
     private void OnPlayGameClick(ClickEvent evt)
     {
         Debug.Log("Presionaste el boton play");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Uno");
+    }
+
+    private void OnOptionsClick(ClickEvent evt)
+    {
+        Debug.Log("Presionaste el boton opciones");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Options");
     }
 }
