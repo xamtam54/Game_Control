e294a0ae27856ca54dc32c1d6a52ae8b25440bfd
xamtam54 <107766970+xamtam54@users.noreply.github.com>
2024-04-30 16:23:44 -0500

    targets

--- Desafio_Del_Agua/Assets/Scripts/S_Targets.cs
+++ Desafio_Del_Agua/Assets/Scripts/S_Targets.cs
@@ -6,6 +6,7 @@ public class S_Targets : MonoBehaviour
 {
 
     public Transform[] targets;
+    public Transform[] devices;
 
     public void S_plants()
     {
@@ -23,5 +24,23 @@ public class S_Targets : MonoBehaviour
 
 
         this.targets = plantTargets.ToArray();
+    }
+
+    public void S_Devices()
+    {
+        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+
+        List<Transform> plantTargets = new List<Transform>();
+
+        foreach (GameObject obj in allObjects)
+        {
+            if (obj.CompareTag("Device"))
+            {
+                plantTargets.Add(obj.transform);
+            }
+        }
+
+
+        this.devices = plantTargets.ToArray();
     }
 }
